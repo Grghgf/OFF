f46853e65775a0efe18509a96193eb9d36ab56ca
DENZO-UCHIWA <145277093+DENZO-UCHIWA@users.noreply.github.com>
2024-11-30 23:41:01 +0100

    𝐁𝐋𝐀𝐒𝐓-𝐌𝐃 BY @DENZO-UCHIWA

--- plugins/search.js
+++ plugins/search.js
@@ -124,7 +124,7 @@ smd({
   );
  
    smd({
-           pattern: "blade",
+           pattern: "pedros",
            category: "search",
            desc: "Finds info about song",
            filename: __filename,
@@ -607,7 +607,7 @@ if(n && n[0]){
       "Accept-Encoding": "application/json",
   };
 
-    const res = await axios.get("https://www.googleapis.com/customsearch/v1?q=", { headers: headers, params: params });
+    const res = await axios.get("https://www.google.com/search?q=", { headers: headers, params: params });
     let body = res.data;
     body = body.slice(body.lastIndexOf("AF_initDataCallback"));
     body = body.slice(body.indexOf("["));
